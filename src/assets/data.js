7a665b3feefd564183fe26e1dd8d6f5d4f7acad3
jmckoy555 <jmckoy555@gmail.com>
2024-02-13 22:38:06 -0500

    initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,45 @@
+export const data = [
+    {
+        question: "What actor plays Ethan Hunt in the Mission Impossible Franchise?",
+        option1: "Matt Damon",
+        option2: "Tom Cruise",
+        option3: "Daniel Craig",
+        option4: "Idris Elba",
+        ans: 2,
+    },{
+        question: "How many Oscars does Daniel Day-Lewis have?",
+        option1: "1",
+        option2: "2",
+        option3: "3",
+        option4: "4",
+        ans: 3,
+    },{
+        question: "Who is the creator, writer and star of the hit ABC comedy Abbott Elementary?",
+        option1: "Issa Rae",
+        option2: "Kenya Barris",
+        option3: "Tyler Perry",
+        option4: "Quinta Brunson",
+        ans: 4,
+    },{
+        question: "What is James Bond's signature drink?",
+        option1: "Dry Martini",
+        option2: "Long Island Ice Tea",
+        option3: "Margarita",
+        option4: "Manhattan",
+        ans: 1,
+    },{
+        question: "What character from The Office is a beet farmer?",
+        option1: "Michael Scott",
+        option2: "Dwight K. Schrute",
+        option3: "Jim Halpert",
+        option4: "Pam Beasley",
+        ans: 2,
+    },{
+        question: "What is the name of the late 90's action flick starring Nicolas Cage and John Travolta?",
+        option1: "True/Lies",
+        option2: "Con Air",
+        option3: "The Rock",
+        option4: "Face/Off",
+        ans: 4,
+    },
+]
